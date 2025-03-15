a02ae1d05b781ed580f5580363b6d2a42c684058
YukaChoco <sugar.ore.yk@gmail.com>
2025-03-15 13:13:55 +0900

    サーバーを 0.0.0.0に変更

--- app/app.py
+++ app/app.py
@@ -14,5 +14,5 @@ app.add_route("/", AppResource())
 
 if __name__ == "__main__":
     from wsgiref import simple_server
-    httpd = simple_server.make_server("127.0.0.1", 8000, app)
+    httpd = simple_server.make_server("0.0.0.0", 8000, app)
     httpd.serve_forever()
