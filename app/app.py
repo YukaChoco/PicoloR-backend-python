4e8053ae5d32719202d8e7b597aa8b47d73edb9a
YukaChoco <sugar.ore.yk@gmail.com>
2025-03-15 12:52:01 +0900

    first commit

---
+++ app/app.py
@@ -0,0 +1,18 @@
+import json
+import falcon
+
+class AppResource(object):
+
+    def on_get(self, req, resp):
+        msg = {
+            "message": "Welcome to the Falcon!!"
+        }
+        resp.text = json.dumps(msg)
+
+app = falcon.App()
+app.add_route("/", AppResource())
+
+if __name__ == "__main__":
+    from wsgiref import simple_server
+    httpd = simple_server.make_server("127.0.0.1", 8000, app)
+    httpd.serve_forever()
